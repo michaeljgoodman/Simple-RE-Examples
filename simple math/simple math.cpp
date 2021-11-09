4a653a93d0e4beb2235f7da3d7fd6df90527bb51
michaelgoodman-cr <michael.goodman@controlrisks.com>
2021-11-09 20:36:58 +0000

    added simple math, and structures

---
+++ simple math/simple math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 7;
+    int z;
+    z = x + y;
+
+    std::cout << "z is " << z << "\n";
+
+    double d = 5.4;
+
+    std::cout << "d is " << d << "\n";
+}
---
+++ structures/structures.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    struct animal {
+        char name[32];
+        char species[32];
+    };
+
+    animal pet1 = {"whiskers", "cat"};
+
+    std::cout << "Pet no. 1's name is " << pet1.name << "\n";
+    std::cout << "Your pets are named: \n";
+
+    animal pets[] = {{"whiskers", "cat"}, {"bruce", "dog"}, {"echo", "velociraptor"}};
+
+    for (animal pet : pets)
+        std::cout<<"Pet name: " << pet.name << "\n";
+    return 0;
+}

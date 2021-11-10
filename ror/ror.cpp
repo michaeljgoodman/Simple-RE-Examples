2ad18f59cf8f7c78f5b34a2a86affb3a62878a98
michaelgoodman-cr <michael.goodman@controlrisks.com>
2021-11-10 23:05:01 +0000

    minor changes

--- ror/ror.cpp
+++ ror/ror.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 const unsigned long cBitsInByte=8; // implementation-defined. It is 8 for most platforms.
 unsigned long ror(unsigned long value,unsigned char rotation)
 {
 return (value<<rotation) | (value>>(sizeof(value)*cBitsInByte - rotation));
 }
-
+string names[] = {"sdf", "wef", "dave", "steve", "sam", "mark", "giles", "joe", "keiran", "tom", "chaz", "laura", "c", "fakename", "ford", "also jack", "jack", "paulo", "sasha", "tim", "jeremy"};
 int main() {
     int nums[] = {5, 4, 3, 8, 9, 1};
     for (int x:nums)
-        std::cout << ror(nums[x],7) << "\n";
-        
-    
+        cout << nums[x] << "\n";
 }

e392b80a675b54e4a4ba3e4d0a4f836025f21cec
michaelgoodman-cr <michael.goodman@controlrisks.com>
2021-11-11 00:02:36 +0000

    added commenting on byte sizes

--- ror/ror.cpp
+++ ror/ror.cpp
@@ -2,8 +2,11 @@
 #include <string>
 
 using namespace std;
+// we define our byte as having 8 bits, but we compile for 32 bit
+// this gives us some interesting results in IDA later
+// a char is 1 byte in this case for reference
 
-const unsigned long cBitsInByte=8; // implementation-defined. It is 8 for most platforms.
+const unsigned long cBitsInByte=8;
 unsigned long ror(unsigned long value,unsigned char rotation)
 {
 return (value<<rotation) | (value>>(sizeof(value)*cBitsInByte - rotation));
@@ -11,7 +14,7 @@ return (value<<rotation) | (value>>(sizeof(value)*cBitsInByte - rotation));
 string names[] = {"sdf", "wef", "dave", "steve", "sam", "mark", "giles", "joe", "keiran", "tom", "chaz", "laura", "c", "fakename", "ford", "also jack", "jack", "paulo", "sasha", "tim", "jeremy"};
 int main() {
     int nums[] = {5, 4, 3, 8, 9, 1};
-    
+
     for (int x:nums)
         for (char c:names[nums[x]])
             cout << ror(c, 2) << "\n";
